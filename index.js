2e8bfddba23eabec5cae4a81b75fa2efd4a86c66
AlisinaAhmadi <94075587+AlisinaAhmadi@users.noreply.github.com>
2022-10-29 02:31:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const App = Vue . createApp({
+    data (){
+        return {
+            message: "",
+            quantity: null,
+            output: null,
+        }
+    },
+    methods: {
+        duplicate() {
+            if (this.quantity < 0) {
+                this.output = `Must be non-negative number: ${this.quantity}`
+            }
+            else {
+                this.output = this.message
+            }
+        }
+    }
+}).mount("#app")
+
